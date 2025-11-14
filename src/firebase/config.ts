7d882a102f101fb5583d6e6d7b82e1607e7860a8
Ashiq Kunchon <ashiqparapurath@gmail.com>
2025-11-14 18:57:53 +0000

    image uploade faild

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-3363182495-f6f10",
   "appId": "1:416904916684:web:eaaec339c5f7a888275c69",
+  "storageBucket": "studio-3363182495-f6f10.appspot.com",
   "apiKey": "AIzaSyDdWh68ZLTE_chcIZWrbCBIpxW9RBPRmdw",
   "authDomain": "studio-3363182495-f6f10.firebaseapp.com",
   "measurementId": "",
