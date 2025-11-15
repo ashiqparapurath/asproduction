ab31104cd72f3daf1f3db22af7c975a7ca027858
Ashiq Kunchon <ashiqparapurath@gmail.com>
2025-11-15 06:32:43 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "asproduction-f4753",
-  "appId": "1:1078333339423:web:44383c7492c3a5951833d7",
-  "storageBucket": "asproduction-f4753.appspot.com",
-  "apiKey": "AIzaSyAzT3yA0tTfGgYh-PIRnc8M1GvoJzWH5iE",
-  "authDomain": "asproduction-f4753.firebaseapp.com",
-  "measurementId": "G-S0EL4L0S53",
-  "messagingSenderId": "1078333339423"
+  "projectId": "studio-3363182495-f6f10",
+  "appId": "1:416904916684:web:eaaec339c5f7a888275c69",
+  "storageBucket": "studio-3363182495-f6f10.appspot.com",
+  "apiKey": "AIzaSyDdWh68ZLTE_chcIZWrbCBIpxW9RBPRmdw",
+  "authDomain": "studio-3363182495-f6f10.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "416904916684"
 };
